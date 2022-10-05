dc6fcb74bc334dfdd26a344aa98216284e9de253
ad-mitium <99030354+ad-mitium@users.noreply.github.com>
2022-10-05 09:02:09 -0400

    Added playable media and colorful notification

--- bash-debug-script.sh
+++ bash-debug-script.sh
@@ -1,15 +1,16 @@
 #/bin/bash
 
-#echo "Executing..."
+echo "Executing..."
 
 #####  Basic parameters  #####
 base_dir=~/Documents/Code/bash-debug-script
 data_dir=data
-start_window=920
+start_window=750
 stop_window=1630
 output_file=bash-debug-script.out
 error_file=bash-debug-script.err
 
+cwd=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 #####  EXECUTING SCRIPT  #####
 if [ $(date +%k%M) -ge $stop_window ] # Note: executing multiple times after stop_window will continue to write to file
@@ -22,7 +23,16 @@ elif [ $(date +%k%M) -lt $start_window ] # Don't execute before this time
   else
   case $(date +%M) in # at certain times, write time to file for debugging
     00)
-        echo $(date +%I:%M:%S) $(date +%p) >> $base_dir/$data_dir/$error_file;;
+        if [ $(date +%k) -gt 9 ] # Don't execute before 0900
+        then
+            echo $(date +%I:%M:%S) $(date +%p) >> $base_dir/$data_dir/$error_file
+        else
+            play -q $cwd/media/beep.wav -t alsa
+            sleep 0.25s
+            play -q $cwd/media/beep.wav -t alsa
+            sleep 0.875s
+        fi    
+        ;;
     15)
         echo $(date +%I:%M:%S) $(date +%p) >> $base_dir/$data_dir/$error_file;;
     20)
@@ -31,9 +41,10 @@ elif [ $(date +%k%M) -lt $start_window ] # Don't execute before this time
         echo $(date +%I:%M:%S) $(date +%p) >> $base_dir/$data_dir/$error_file;;
     40)
         echo $(date +%I:%M:%S) $(date +%p) >> $base_dir/$data_dir/$error_file;;
-    45)
+    45|55)
         echo $(date +%I:%M:%S) $(date +%p) >> $base_dir/$data_dir/$error_file;;
     *)
+        echo -e "    \033[1;33m\033[43  Nothing to do at "$(date +%I)":"$(date +%M)":"$(date +%S)"    \033[0m"
         ;;
   esac
 
@@ -42,6 +53,6 @@ elif [ $(date +%k%M) -lt $start_window ] # Don't execute before this time
 #    <SOME APPLICATION TO DEBUG> 1>>$base_dir/$data_dir/$output_file 2>>$base_dir/$data_dir/$error_file
 
 # Example:
-    date 1>>$base_dir/$data_dir/$output_file 2>>$base_dir/$data_dir/$error_file
-
+#    date 1>>$base_dir/$data_dir/$output_file 2>>$base_dir/$data_dir/$error_file
+    play -q $cwd/media/beep.wav -t alsa
 fi
