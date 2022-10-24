6d1af3a51df7199071c341b1458bdc8612c30e81
ad-mitium <99030354+ad-mitium@users.noreply.github.com>
2022-10-24 13:10:37 -0400

    Moved ';;' around, fixed $ location in tee

--- bash-debug-script.sh
+++ bash-debug-script.sh
@@ -16,8 +16,8 @@ cwd=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 #####  EXECUTING SCRIPT  #####
 if [ $(date +%k%M) -ge $stop_window ] # Note: executing multiple times after stop_window will continue to write to file
 then
-  echo 'Testing was completed at' $(date +%a) $(date +%D) $(date +%I:%M:%S) $(date +%p) >> $base_dir/$data_dir/$output_file
-  echo 'Testing was completed at'$(date) >> $base_dir/$data_dir/$error_file
+  echo 'Testing was completed at '$(date +%a) $(date +%D) $(date +%I:%M:%S) $(date +%p) >> $base_dir/$data_dir/$output_file
+  echo 'Testing was completed at '$(date) >> $base_dir/$data_dir/$error_file
 elif [ $(date +%k%M) -lt $start_window ] # Don't execute before this time 
   then
     exit 100
@@ -37,17 +37,19 @@ elif [ $(date +%k%M) -lt $start_window ] # Don't execute before this time
     15)
         echo $(date +%I:%M:%S) $(date +%p) >> $base_dir/$data_dir/$error_file;;
     30|40)
-        echo $(date +%I:%M:%S) $(date +%p) >> $base_dir/$data_dir/$error_file;;
+        echo $(date +%I:%M:%S) $(date +%p) >> $base_dir/$data_dir/$error_file
         i=0
         while [ $i -lt 3 ]
             do  
                 play -q $cwd/media/beep.wav -t alsa
                 ((i++))
             done
+        ;;
     *0)
-        echo $(date +%I:%M:%S) $(date +%p) >> $base_dir/$data_dir/$error_file;;
+        echo $(date +%I:%M:%S) $(date +%p) >> $base_dir/$data_dir/$error_file
         sleep 0.875s
         play -q $cwd/media/beep.wav -t alsa
+        ;;
     *5)
         echo $(date +:%M:%S) $(date +%p) >> $base_dir/$data_dir/$error_file;;
     *)
@@ -65,4 +67,4 @@ elif [ $(date +%k%M) -lt $start_window ] # Don't execute before this time
 fi
 
 #####  Timestamp output and error files  #####
-date | /usr/bin/tee -a $home_dir/$base_dir/$data_dir/${error_file,output_file} #bash shell needed for this line
+date | /usr/bin/tee -a $base_dir/$data_dir/{$output_file,$error_file} #bash shell needed for this line
